c30f6bea1e629dc76fddff63dc1e9c0751200c79
James Chuang <james.chuang@socrata.com>
2016-05-24 17:05:24 -0700

    NO-TICKET: add permit information

---
+++ lib/spy_glass/registry/douglas-county-permit-information.rb
@@ -0,0 +1,45 @@
+require 'spy_glass/registry'
+
+time_zone = ActiveSupport::TimeZone["Pacific Time (US & Canada)"]
+
+opts = {
+  path: '/douglas-county-permit-info',
+  cache: SpyGlass::Cache::Memory.new(expires_in: 300),
+  source: 'https://data.douglas.co.us/resource/bedb-m69t.json?'+Rack::Utils.build_query({
+    '$limit' => 1500
+  })
+}
+
+SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
+  features = collection.map do |item|
+
+    case item['location']
+    when nil
+    else
+      longitude = item['location']['coordinates'][0].to_f
+      latitude = item['location']['coordinates'][1].to_f
+    end
+
+    title = <<-TITLE
+      A(n) $#{item['permit_job_type'].to_s} was filed for $#{item['original_address_1'].to_s} on $#{item['applied_date'].to_s} for $#{item['description'].to_s}
+      Status: $#{item['status_current'].to_s}
+      Proposed Value: $#{item['job_valuation'].to_s}
+    TITLE
+
+    {
+      'id' => item['permit_number'],
+      'type' => 'Feature',
+      'geometry' => {
+        'type' => 'Point',
+        'coordinates' => [
+          longitude,
+          latitude
+        ]
+      },
+      'properties' => item.merge('title' => title)
+    }
+  end
+
+  {'type' => 'FeatureCollection', 'features' => features}
+end
+
