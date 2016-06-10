a9f1eaa328c6fe6c279acc46e61b1504289dd3e5
James Chuang <james.chuang@socrata.com>
2016-06-10 16:29:12 -0700

    NO-TICKET: update 4x4 for cone zone

--- lib/spy_glass/registry/douglas-county-cone-zone.rb
+++ lib/spy_glass/registry/douglas-county-cone-zone.rb
@@ -5,7 +5,7 @@ time_zone = ActiveSupport::TimeZone["Pacific Time (US & Canada)"]
 opts = {
   path: '/douglas-county-cone-zone',
   cache: SpyGlass::Cache::Memory.new(expires_in: 300),
-  source: 'https://data.douglas.co.us/resource/e9ij-7j7y.json?'+Rack::Utils.build_query({
+  source: 'https://data.douglas.co.us/resource/8kwh-cx7b.json?'+Rack::Utils.build_query({
     '$limit' => 1500
   })
 }
@@ -18,7 +18,7 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
       # latlng = item['shape'].gsub(/[()]/, '').split(/\s*,\s*/)
       # longitude = latlng[1].to_f
       # latitude = latlng[0].to_f
-      line = item['shape']['geometry']['paths'][0]
+      line = item['shape']['geometry']['paths']
     end
 
     title = <<-TITLE
