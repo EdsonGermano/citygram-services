27ec00b5d15aa59d23026b5e1d019e4f742182cf
James Chuang <james.chuang@socrata.com>
2016-07-28 02:22:07 -0700

    CS-8466: only show permits with status ISSUED

--- lib/spy_glass/registry/douglas-county-permit-information.rb
+++ lib/spy_glass/registry/douglas-county-permit-information.rb
@@ -13,32 +13,33 @@ opts = {
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
   features = collection.map do |item|
+    if item['status_current'] == "ISSUED"
+      case item['location']
+      when nil
+      else
+        longitude = item['location']['longitude'].to_f
+        latitude = item['location']['latitude'].to_f
+      end
 
-    case item['location']
-    when nil
-    else
-      longitude = item['location']['longitude'].to_f
-      latitude = item['location']['latitude'].to_f
+      title = <<-TITLE
+        #{item['permit_job_type'].to_s.indefinite_article.capitalize} #{item['permit_job_type'].to_s} Permit was filed for #{item['original_address_1'].to_s} on #{Time.at(item['applied_date']).strftime("%m-%d-%Y")} for #{item['description'].to_s}
+        Status: #{item['status_current'].to_s}
+        Proposed Value: #{Money.us_dollar(item['job_valuation'].to_f * 100).format(:no_cents_if_whole => true)}
+      TITLE
+
+      {
+        'id' => item['permit_number'],
+        'type' => 'Feature',
+        'geometry' => {
+          'type' => 'Point',
+          'coordinates' => [
+            longitude,
+            latitude
+          ]
+        },
+        'properties' => item.merge('title' => title)
+      }
     end
-
-    title = <<-TITLE
-      #{item['permit_job_type'].to_s.indefinite_article.capitalize} #{item['permit_job_type'].to_s} Permit was filed for #{item['original_address_1'].to_s} on #{Time.at(item['applied_date']).strftime("%m-%d-%Y")} for #{item['description'].to_s}
-      Status: #{item['status_current'].to_s}
-      Proposed Value: #{Money.us_dollar(item['job_valuation'].to_f * 100).format(:no_cents_if_whole => true)}
-    TITLE
-
-    {
-      'id' => item['permit_number'],
-      'type' => 'Feature',
-      'geometry' => {
-        'type' => 'Point',
-        'coordinates' => [
-          longitude,
-          latitude
-        ]
-      },
-      'properties' => item.merge('title' => title)
-    }
   end
 
   {'type' => 'FeatureCollection', 'features' => features}
